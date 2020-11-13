486e6513910a5f6fba30059cdf1e645657925fd2
edd <zeedymarina@gmail.com>
2020-11-13 11:16:00 -0500

    1

--- src/test/java/fistClass/class_1.java
+++ src/test/java/fistClass/class_1.java
@@ -1,6 +1,8 @@
 package fistClass;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -22,7 +24,25 @@ public class class_1 {
         String actual = driveN.getTitle();
         String newtitle = "Facebook - Log In or Sign Up";
         Assert.assertEquals(actual, newtitle, "or rrong");
-        driveN.quit();
+       // driveN.quit();
+
+        // email Locator By.id
+        String logIn = "email";
+        By logInName = By.id(logIn);
+        WebElement emailIn = driveN.findElement(logInName);
+        emailIn.sendKeys("email@gmail.com");
+
+        // password locator By.name
+        String logPass = "pass";
+        By logInPass = By.name(logPass);
+        WebElement PassIn = driveN.findElement(logInPass);
+        PassIn.sendKeys("1234");
+
+        // click button Locator By.name
+        String logClick = "login";
+        By logInClick = By.name(logClick);
+        WebElement clickIn = driveN.findElement(logInClick);
+        clickIn.click();
 
     }
 
